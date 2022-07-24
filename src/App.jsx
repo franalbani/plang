4f0e90f5d49754994e47761cada67835af83b8c6
franalbani <franalbani@users.noreply.github.com>
2022-07-24 01:10:55 -0300

    boceto de un patrón en html simple

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,91 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to React!</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+            <h3>
+                    The elements of this language are entities called patterns.
+                    Each pattern describes a problem which occurs over and over
+                    again in our environment, and then describes the core of 
+                    the solution to that problem, in such a way that you 
+                    can use this solution a million times over,
+                    without ever doing it the same way twice.
+            </h3>
+      <h1>
+      105 South facing outdoors
+      </h1>
+      <hr/>
+      <h3>
+              each pattern has an introductory paragraph,
+              which sets the context for the pattern,
+              by explaining how it helps to complete certain larger patterns
+      </h3>
+      <div>
+              <i>
+              within the general ideas of location which
+              <a href=''> 104 Site Repair</a> creates,
+              this pattern governs the fundamental placing of the building
+              and the open space around it with respect to sun.
+              </i>
+      </div>
+      <hr/>
+      <h3>This headline gives the essence of the problem in one or two sentences.</h3>
+      <h2><center>
+                      <b>People use open space if it is sunny, and do not use it if it isn’t, in all but desert climates.</b>
+              </center>
+      </h2>
+      <hr/>
+      <h3>
+              After the headline comes the body of the problem.
+              This is the longest section. It describes the empirical background of the pattern,
+              the evidence for its validity, the range of different ways 
+              the pattern can be manifested in a building, and so on.
+      </h3>
+      <div>
+              This is perhaps the most important single fact about a building.
+              If the building is placed right, the building and its gardens
+              will be happy places full of activity and laughter.
+              If it is done wrong, then all the attention in the world,
+              and the most beautiful details, will not prevent it from 
+              being a silent gloomy place.
+              Thousands of acres of open space in every city are wasted 
+              because they are north of buildings and never get the sun.
+      </div>
+      <hr/>
+      <h3>
+              Then is the solution—the heart of the pattern—which describes 
+              the field of physical and social relationships which are required 
+              to solve the stated problem, in the stated context. 
+              This solution is always stated in the form of an instruction
+              —so that you know exactly what you need to do, to build the pattern.
+      </h3>
+      <h2><center>
+              <b>
+              Always place buildings to the north of the outdoor spaces 
+              that go with them, and keep the outdoor spaces to the south.
+              Never leave a deep band of shade between the building 
+              and the sunny part of the outdoors.</b>
+              </center>
+      </h2>
+      <hr/>
+      <div>
+              <h3>
+                      And finally, after the diamonds there is a paragraph
+                      which ties the pattern to all those smaller patterns
+                      in the language, which are needed to complete this pattern,
+                      to embellish it, to fill it out.
+              </h3>
+              <ul>
+                      <li>Let <a href=''>HALF-HIDDEN GARDEN (111)</a> influence the position of the outdoors too.</li>
+                      <li>Make the outdoor spaces positive—<a href=''>POSITIVE OUTDOOR SPACE (106)</a></li>
+                      <li>—and break the building into narrow wings—<a href=''>WINGS OF LIGHT (107)</a>.</li>
+                      <li>Keep the most important rooms to the south of these wings—<a href=''>INDOOR SUNLIGHT (128)</a>;</li>
+                      <li>and keep storage, parking, etc, to the north—<a href=''>NORTH FACE (162)</a>.</li>
+                      <li>When the building is more developed, you can concentrate on the
+                          special sunny areas where the outdoors and building meet,
+                          and make definite places there, where people can sit in the sun—<a href=''>SUNNY PLACE (161)</a>.</li>
+          </ul>
+      </div>
     </div>
   );
 }
