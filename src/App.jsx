2ed7d326b7b560f7ca8d114aa83b99cbedff4583
franalbani <franalbani@users.noreply.github.com>
2022-07-26 00:41:55 -0300

    agrego ruteo y reactifico más el Pattern

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,136 @@
 import "./App.css";
 
-function Pattern(){
-        const number = 105
-        const title = 'South facing outdoors'
-        const larger_patterns = [{num: 104, title: 'Site repair', relation: 'within the general ideas of location which 104 Site Repair creates, this pattern governs the fundamental placing of the building and the open space around it with respect to sun'},]
+const PATTERNS = {
 
-        const essence = 'People use open space if it is sunny, and do not use it if it isn’t, in all but desert climates.'
-        const body = `
-            This is perhaps the most important single fact about a building.
-            If the building is placed right, the building and its gardens
-            will be happy places full of activity and laughter.
-            If it is done wrong, then all the attention in the world,
-            and the most beautiful details, will not prevent it from 
-            being a silent gloomy place.
-            Thousands of acres of open space in every city are wasted 
-            because they are north of buildings and never get the sun.
-            `
-        const solution = `
-            Always place buildings to the north of the outdoor spaces 
-            that go with them, and keep the outdoor spaces to the south.
-            Never leave a deep band of shade between the building 
-            and the sunny part of the outdoors.
-            `
-        const smaller_patterns = [{num: 111, title: 'Half-hidden garden', relation: 'let it influence the position of the outdoors too'},
-                {num: 106, title: 'Positive outdoor space', relation: 'make the outdoor spaces positive'},
-                {num: 107, title: 'Wings of light', relation: 'break the building into narrow wings'},
-                {num: 128, title: 'Indoor sunlight', relation: 'keep the most important rooms to the south'},
-                {num: 162, title: 'North face', relation: 'keep storage, parking, etc, to the north'},
-                {num: 161, title: 'Sunny place', relation: 'When the building is more developed, you can concentrate on the special sunny areas where the outdoors and building meet, and make definite places there, where people can sit in the sun'},]
+    95: {title: 'Building complex'},
+    104: {
+            title: 'Site repair',
+            essence: 'Buildings must always be built on those parts of the land which are in the worst condition, not the best.',
+            body:   `
+                    pending
+                    `,
+            solution:   `
+                        On no account place buildings in the places which are most beautiful.
+                        In fact, do the opposite.
+                        Consider the site and its buildings as a single living eco-system.
+                        Leave those areas that are the most precious, beautiful, comfortable,
+                        and healthy as they are, and build new structures in those parts of the site which are least pleasant now.
+                        `,
+            larger_patterns: {
+                    95: 'breaks buildings into smaller parts, and therefore makes it possible to tuck them into different corners of the site in the best places.',
+            },
+
+            smaller_patterns: {
+                    171: 'Above all, leave trees intact and build around them with great care.',
+                    105: 'keep open spaces open to the south of buildings, for the sun.',
+                    106: 'try, generally, to shape space in such a way that each place becomes positive, in its own right.',
+                    169: 'repair slopes if they need it with this.',
+                    172: 'leave the outdoors in its natural state as much as possible.',
+                    107: 'If necessary, push and shove the building into odd corners to preserve the beauty of an old vine, a bush you love, a patch of lovely grass.',
+                    109: 'ídem 107',
+                            },
+
+    },
+    105: {
+            title: 'South facing outdoors',
+            essence: 'People use open space if it is sunny, and do not use it if it isn’t, in all but desert climates.',
+            body:   `
+                    This is perhaps the most important single fact about a building.
+                    If the building is placed right, the building and its gardens
+                    will be happy places full of activity and laughter.
+                    If it is done wrong, then all the attention in the world,
+                    and the most beautiful details, will not prevent it from 
+                    being a silent gloomy place.
+                    Thousands of acres of open space in every city are wasted 
+                    because they are north of buildings and never get the sun.
+                    `,
+            solution:   `
+                        Always place buildings to the north of the outdoor spaces 
+                        that go with them, and keep the outdoor spaces to the south.
+                        Never leave a deep band of shade between the building 
+                        and the sunny part of the outdoors.
+                        `,
+            larger_patterns: {
+                    104: 'within the general ideas of location which 104 Site Repair creates, this pattern governs the fundamental placing of the building and the open space around it with respect to sun'
+                            }
+                            ,
+            smaller_patterns: {
+                    111: 'let it influence the position of the outdoors too',
+                    106: 'make the outdoor spaces positive',
+                    107: 'break the building into narrow wings',
+                    128: 'keep the most important rooms to the south',
+                    162: 'keep storage, parking, etc, to the north',
+                    161: 'When the building is more developed, you can concentrate on the special sunny areas where the outdoors and building meet, and make definite places there, where people can sit in the sun',
+                            },
+
+    },
+    106: {title: 'Positive outdoor space'},
+    107: {title: 'Wings of light'},
+    109: {title: 'Long thin house'},
+    111: {title: 'Half-hidden garden'},
+    128: {title: 'Indoor sunlight'},
+    161: {title: 'Sunny place'},
+    162: {title: 'North face'},
+    169: {title: 'Terraced slope'},
+    171: {title: 'Tree places'},
+    172: {title: 'Garden growing wild'},
+}
+
+
+function Pattern({ pattern }){
 
     return (
         <div>
-            <h1>{number} {title}</h1>
+            <h1>{pattern} {PATTERNS[pattern].title}</h1>
             <hr/>
+            {'larger_patterns' in PATTERNS[pattern]
+                            &&
             <ul style={{fontFamily: 'monospace'}}>
-                    {larger_patterns.map(e => <li key={e.num}><a href=''><b>{e.num}</b> {e.title}</a>: {e.relation}</li>)}
+                    {Object.entries(PATTERNS[pattern].larger_patterns).map(
+                            ([k, v]) => <li key={k}><a href={k}><b>{k}</b> {PATTERNS[k].title} </a>: {v}</li>)}
             </ul>
+                            ||
+                            <span>nada</span>
+            }
             <hr/>
-            <h2><center><b>{essence}</b></center></h2>
+            {'essence' in PATTERNS[pattern]
+                            &&
+            <h2><center><b>{PATTERNS[pattern].essence}</b></center></h2>
+                            ||
+                            <span>nada</span>
+            }
             <hr/>
-            <div>{body}</div>
+            {'body' in PATTERNS[pattern]
+                            &&
+            <div>{PATTERNS[pattern].body}</div>
+                            ||
+                            <span>nada</span>
+            }
             <hr/>
-            <h2><center><b>{solution}</b></center></h2>
+            {'solution' in PATTERNS[pattern]
+                            &&
+            <h2><center><b>{PATTERNS[pattern].solution}</b></center></h2>
+                            ||
+                            <span>nada</span>
+            }
             <hr/>
+            {'smaller_patterns' in PATTERNS[pattern]
+                            &&
             <ul style={{fontFamily: 'monospace'}}>
-                    {smaller_patterns.map(e => <li key={e.num}><a href=''><b>{e.num}</b> {e.title}</a>: {e.relation}</li>)}
+                    {Object.entries(PATTERNS[pattern].smaller_patterns).map(
+                            ([k, v]) => <li key={k}><a href={k}><b>{k}</b> {PATTERNS[k].title} </a>: {v}</li>)}
             </ul>
+                            ||
+                            <span>nada</span>
+            }
         </div>
     )
 
 }
+
 function App() {
+
+  const pattern = parseInt(window.location.pathname.slice(1)) || 104
   return (
     <div style={{display:'flex'}}>
             <div style={{flex:'25%', borderRight: '3px solid blue'}}>
@@ -90,9 +170,10 @@ function App() {
               </h3>
       </div>
       </div>
-            <div style={{flex:'75%'}}>
-                    <Pattern />
+      <div style={{flex:'75%'}}>
+              {pattern in PATTERNS && <Pattern pattern={pattern} /> || <span>pattern {pattern} not found</span>}
       </div>
+      <hr/>
     </div>
   );
 }
